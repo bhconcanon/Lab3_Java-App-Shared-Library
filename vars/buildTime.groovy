c4f933cd625f21b8c1291bf476c303345effce82
bhconcanon <bilal.hussain@concanon.com>
2021-10-04 16:05:45 -0400

    Created shared library for build times

---
+++ vars/buildTime.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call(String name = 'build') {
+
+	println "Last Run: " + build.getTimestampString()
+	println "Last Run Date and Time: " + build.getTimestampString2()
+	println "Build Time In Ms: " + build.running duration()
+	println "End time: " + new Date(((long)build.getStartTimeInMillis() + build.duration))
+}
