9dce1a72825363e4638dac77dfe5e8d08cad75cd
bhconcanon <bilal.hussain@concanon.com>
2021-10-05 14:09:49 -0400

    Changed println to echo

--- vars/buildTime.groovy
+++ vars/buildTime.groovy
@@ -2,8 +2,8 @@
 
 def call(Map config=[:]) {
 
-	println "Last Run: " + build.getTimestampString()
-	println "Last Run Date and Time: " + build.getTimestampString2()
-	println "Build Time In Ms: " + build.running duration()
-	println "End time: " + new Date(((long)build.getStartTimeInMillis() + build.duration))
+	echo "Last Run: " + build.getTimestampString()
+	echo "Last Run Date and Time: " + build.getTimestampString2()
+	echo "Build Time In Ms: " + build.running duration()
+	echo "End time: " + new Date(((long)build.getStartTimeInMillis() + build.duration))
 }
