c50bbc996334d625149d5adcf04f5421179eb26a
bhconcanon <bilal.hussain@concanon.com>
2021-10-05 14:21:30 -0400

    test

--- vars/buildTime.groovy
+++ vars/buildTime.groovy
@@ -2,10 +2,7 @@
 
 def call(def config=[:]) {
 
-	echo "hello world"
-/*	echo "Last Run: " + build.getTimestampString()
+	echo "Last Run: " + build.getTimestampString()
 	echo "Last Run Date and Time: " + build.getTimestampString2()
-	echo "Build Time In Ms: " + build.running duration()
 	echo "End time: " + new Date(((long)build.getStartTimeInMillis() + build.duration))
-*/	
 }
