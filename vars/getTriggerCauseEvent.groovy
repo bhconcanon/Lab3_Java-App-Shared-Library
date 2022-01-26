c6a5df60119885eb76a1336846d6a767cc40241e
bhconcanon <bilal.hussain@concanon.com>
2022-01-26 13:50:44 -0500

    Added shared library for test trigger

--- vars/getTriggerCauseEvent.groovy
+++ vars/getTriggerCauseEvent.groovy
@@ -1,11 +1,8 @@
 String getTriggerCauseEvent() {
     def buildCauseInfo = currentBuild.getBuildCauses("com.cloudbees.jenkins.plugins.pipeline.events.EventTriggerCause")
-    println "retrieving payload"
     if (buildCauseInfo && buildCauseInfo[0])  {
-        println "inside of conditional statement"
-        String branch = buildCauseInfo[0].event.branch
-        println "${branch}"
-        return branch        
+        def test = buildCauseInfo[0].event.test
+        return test
     }
     return "N/A"
 }
